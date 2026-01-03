7394d7792570be1280662e3f75f3fdfd5eb3ef0a
Jonathing <me@jonathing.me>
2026-01-03 11:26:50 -0500

    Strip out the entire plugin

--- renamer-gradle/src/main/java/net/minecraftforge/renamer/gradle/RenamerTask.java
+++ renamer-gradle/src/main/java/net/minecraftforge/renamer/gradle/RenamerTask.java
@@ -1,4 +1,4 @@
-package net.minecraftforge.renamer.gradle.internal;
+package net.minecraftforge.renamer.gradle;
 
 import net.minecraftforge.gradleutils.shared.EnhancedPlugin;
 import net.minecraftforge.gradleutils.shared.EnhancedTask;
